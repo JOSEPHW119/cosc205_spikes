5492ae96277ae41f8b9d56fb9fed35d5f4d6e449
jaxrosco <jax.rosco@gmail.com>
2024-01-26 13:44:21 -0800

    Added some functionality to the form

--- UserDataForm/UserDataForm/Form1.vb
+++ UserDataForm/UserDataForm/Form1.vb
@@ -1,9 +1,24 @@
-﻿Public Class Form1
+﻿Option Strict On
+Public Class Form1
+
     Private Sub FormLoad(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Private Sub Submit(sender As Object, e As EventArgs) Handles submitBTN.Click
+        If emailTBX.Text.Equals("") Or ageTBX.Text.Equals("") Or heightTBX.Text.Equals("") Or weightTBX.Text.Equals("") _
+            Or genderCBX.Text.Equals("") Or goalsCBX.Text.Equals("") Then
+            MessageBox.Show("Please fill out all fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        Else
+            Dim email As String = emailTBX.Text
+            Dim age As Integer = CInt(ageTBX.Text)
+            Dim height As Integer = CInt(heightTBX.Text)
+            Dim weight As Integer = CInt(weightTBX.Text)
+            Dim gender As String = genderCBX.Text
+            Dim goal As String = goalsCBX.Text
 
+            ' Insert data into database
+        End If
     End Sub
 End Class
