752cd78744c89b280c9b52d50b99e4f46cec041c
molypoly <sharmaamol317@gmail.com>
2024-01-29 14:24:41 -0800

    committing new add meal form

--- 205 Lab 2 Add Meal Form/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Create a form that adds meals to the user's profile. (2 hours) Amol
-    End Sub
-
-End Class
