4a0f96b9588796ac87df66bb0f328aa6eeebd00f
molypoly <sharmaamol317@gmail.com>
2024-01-29 21:02:35 -0800

    removing add meal form

--- UserDataForm/UserDataForm/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub FormLoad(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Submit(sender As Object, e As EventArgs) Handles submitBTN.Click
-
-    End Sub
-End Class
